82d462d056227fd19e7d00c1a2a141b039ebb97f
Hein Htet Aung <57698984+hellboyhha@users.noreply.github.com>
2023-07-27 03:50:08 +0630

    Delete calico-addon-installation.sh

--- calico-addon-installation.sh
+++
@@ -1,5 +0,0 @@
-#  install Calico Networking
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
-
-# Check the status of the control plane node
-kubectl get nodes
