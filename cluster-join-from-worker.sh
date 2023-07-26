e06316e8809681488ecee6232cf9c1bb8c5f7766
Hein Htet Aung <57698984+hellboyhha@users.noreply.github.com>
2023-07-27 03:50:35 +0630

    Delete cluster-join-from-worker.sh

--- cluster-join-from-worker.sh
+++
@@ -1,7 +0,0 @@
-# Copy the full output from kubeadm join command used in the control plane node.
-
-# In worker nodes, paste the full kubeadm join command to join the cluster. Use sudo to run it as root.
-# sudo kubeadm join...
-
-# In the control plane node, view the cluster status
-# kubectl get nodes
